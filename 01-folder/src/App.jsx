574adb613afbf901538d35ce0510e25c294734b9
Jay Gupta <jaygupta4520@gmail.com>
2026-03-18 17:21:40 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Chacha</h1>
+      </div>
+      <div>
+        <h3>
+          bap
+        </h3>
+      </div>
+    </>
+  )
+}
+
+export default App
